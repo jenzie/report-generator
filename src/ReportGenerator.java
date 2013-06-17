ddcec0ce4346ef8af31d4cf76b6300d0a7471c37
Jenny Zhen <coffee@csh.rit.edu>
2013-06-17 17:57:59 -0400

    Started working on the report generation.

---
+++ src/ReportGenerator.java
@@ -0,0 +1,63 @@
+/**
+ * @author: Jenny Zhen; jenny.zhen@rit.edu
+ * date: 06.17.2013
+ * language: Java
+ * project: report_generator
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReportGenerator {
+	private Scanner input;
+	private PrintWriter out;
+	private ArrayList<String> data;
+
+	public ReportGenerator(String file) {
+		try {
+			input = new Scanner(new File(file)); // directory, file
+		} catch (FileNotFoundException e) {
+			System.err.println("Usage: java report-generator.java INPUT.txt");
+			System.exit(0);
+		}
+		parseFile();
+	}
+
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			System.err.println("Usage: java report-generator.java INPUT.txt");
+			System.exit(0);
+		}
+		new ReportGenerator(args[0]);
+	}
+
+	private void parseFile() {
+		String result = "";
+		this.data = new ArrayList<String>();
+
+		while(input.hasNextLine())
+			this.data.add(input.nextLine());
+
+		if(data.size() < 12) {
+			System.err.println(
+				"Error: Invalid input file.\n" +
+					"Example:\n" +
+					"\ttime\n\ttime\n\ttime\n" +
+					"\tlowest memory\n\taverage memory\n\thighest memory\n" +
+					"\tlowest memory\n\taverage memory\n\thighest memory\n" +
+					"\tlowest memory\n\taverage memory\n\thighest memory\n");
+			System.exit(0);
+		}
+
+		for(int i = 0; i < 3; i++)
+			result += parseTimeCalculations(data.get(i));
+
+		for(int i = 3; i < 12; i++)
+			result += parseMemoryCalculations(data.get(i));
+	}
+
+
+}
