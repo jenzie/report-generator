be66d8e476191f0f5ec1bf3f7c2b714665db8f5e
Jenny Zhen <coffee@csh.rit.edu>
2013-06-18 10:37:02 -0400

    Worked on generation for memory usage.

--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReportGenerator {
@@ -16,22 +17,24 @@ public class ReportGenerator {
 	private PrintWriter out;
 	private ArrayList<String> data;
 
-	public ReportGenerator(String file) {
+	public ReportGenerator(String inputFile, String outputFile) {
 		try {
-			input = new Scanner(new File(file)); // directory, file
+			input = new Scanner(new File(inputFile));
 		} catch (FileNotFoundException e) {
-			System.err.println("Usage: java report-generator.java INPUT.txt");
+			System.err.println(
+				"Usage: java report-generator.java INPUT.txt OUTPUT.txt");
 			System.exit(0);
 		}
 		parseFile();
 	}
 
 	public static void main(String[] args) {
-		if(args.length != 1) {
-			System.err.println("Usage: java report-generator.java INPUT.txt");
+		if(args.length != 2) {
+			System.err.println(
+				"Usage: java report-generator.java INPUT.txt OUTPUT.txt");
 			System.exit(0);
 		}
-		new ReportGenerator(args[0]);
+		new ReportGenerator(args[0], args[1]);
 	}
 
 	private void parseFile() {
@@ -52,17 +55,58 @@ public class ReportGenerator {
 			System.exit(0);
 		}
 
-		parseTimeCalculations(data.get(0), data.get(1), data.get(2));
-
-		for(int i = 3; i < 12; i++)
-			result += parseMemoryCalculations(data.get(i));
+		parseTimeCalculations(data.subList(0, 3));
+		parseMemoryCalculations(data.subList(3, data.size()));
 	}
 
-	private String parseTimeCalculations(
-			String time1, String time2, String time3) {
-		String results;
+	private String parseTimeCalculations(List<String> times) {
+		String results = " ";
+		String divider = String.format("%-60s", "|").replace(' ', '-') + "|\n";
 
-		results = "------------------------------------------------------------\n";
-		results += "| Time                                                     |\n";
+		results += String.format("%60s", "\n").replace(' ', '-')
+			+ String.format("%-60s", "| Time (milliseconds)")
+				+ "|\n" + divider
+			+ String.format("%-60s", "| Naive Strategy  | " + times.get(0))
+				+ "|\n" + divider
+			+ String.format("%-60s", "| Random Strategy | " + times.get(1))
+				+ "|\n" + divider
+			+ String.format("%-60s", "| Loop Strategy   | " + times.get(2))
+				+ "|\n "
+			+ String.format("%60s", "\n").replace(' ', '-');
+		System.out.println(results);
+		return results;
+	}
+
+	private String parseMemoryCalculations(List<String> memories) {
+		String results = " ";
+		String divider = String.format("%-60s", "|").replace(' ', '-') + "|\n";
+
+		results += String.format("%60s", "\n").replace(' ', '-')
+			+ String.format("%-60s", "| Memory (megaflops)")
+				+ "|\n" + divider + "|"
+			+ String.format("%59s", "| Base        | Average     | Peak        ")
+				+ "|\n" + divider
+			+ String.format("%-18s", "| Naive Strategy") + "| "
+				+ String.format("%-12s", memories.get(0)) + "| "
+				+ String.format("%-12s", memories.get(1)) + "| "
+				+ String.format("%-12s", memories.get(2))
+					+ "|\n" + divider
+			+ String.format("%-18s", "| Random Strategy") + "| "
+				+ String.format("%-12s", memories.get(3)) + "| "
+				+ String.format("%-12s", memories.get(4)) + "| "
+				+ String.format("%-12s", memories.get(5))
+					+ "|\n" + divider
+			+ String.format("%-18s", "| Loop Strategy") + "| "
+				+ String.format("%-12s", memories.get(6)) + "| "
+				+ String.format("%-12s", memories.get(7)) + "| "
+				+ String.format("%-12s", memories.get(8))
+					+ "|\n "
+			+ String.format("%60s", "\n").replace(' ', '-');
+		System.out.println(results);
+		return results;
+	}
+
+	private void writeResults(String outFile, String results) {
+
 	}
 }
