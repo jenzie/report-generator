b97dc6cc1971d0f5ba05572e893cbb0caeaa9ac4
Jenny Zhen <coffee@csh.rit.edu>
2013-06-18 12:02:29 -0400

    Commented the code.

--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -10,12 +10,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * ReportGenerator.
+ * Generates reports for the xml-parser.
+ */
 public class ReportGenerator {
-	private Scanner input;
-	private PrintWriter out;
-	private ArrayList<String> data;
+	private Scanner input; // reads input
+	private PrintWriter out; // writes output
 
+	/**
+	 * Constructor.
+	 * Checks if arguments are valid files; exits, if false.
+	 * @param inputFile file with input data
+	 * @param outputFile file to write results to
+	 */
 	public ReportGenerator(String inputFile, String outputFile) {
+
+		// create reader for input file
 		try {
 			input = new Scanner(new File(inputFile));
 		} catch (FileNotFoundException fnfe) {
@@ -23,6 +34,8 @@ public class ReportGenerator {
 				"Usage: java report-generator.java INPUT.txt OUTPUT.txt");
 			System.exit(0);
 		}
+
+		// create writer for output file
 		try {
 			out = new PrintWriter(new FileWriter(new File(outputFile)));
 		} catch (IOException ioe) {
@@ -30,10 +43,15 @@ public class ReportGenerator {
 					"Could not create print writer for /results/" + outputFile);
 			System.exit(0);
 		}
+
+		// produce and write output
 		parseFile();
-		// System.out.println("Report written to /results/" + outputFile);
 	}
 
+	/**
+	 * Checks the argument count before proceeding with the program.
+	 * @param args the input/output file directories and names.
+	 */
 	public static void main(String[] args) {
 		if(args.length != 2) {
 			System.err.println(
@@ -43,13 +61,18 @@ public class ReportGenerator {
 		new ReportGenerator(args[0], args[1]);
 	}
 
+	/**
+	 * Extracts data from the input file and calls functions to get the result.
+	 */
 	private void parseFile() {
 		String result = "";
-		this.data = new ArrayList<String>();
+		ArrayList<String> data = new ArrayList<String>();
 
+		// extract data from file by line
 		while(input.hasNextLine())
-			this.data.add(input.nextLine());
+			data.add(input.nextLine());
 
+		// ensure validity of input file
 		if(data.size() < 12) {
 			System.err.println(
 				"Error: Invalid input file.\n" +
@@ -61,18 +84,28 @@ public class ReportGenerator {
 			System.exit(0);
 		}
 
+		// get results
 		result += "I. Result.\n\n";
 		result += "II. Time.\n";
 		result += parseTimeCalculations(data.subList(0, 3));
 		result += "III. Base, Average, and Peak Memory.\n";
 		result += parseMemoryCalculations(data.subList(3, data.size()));
+
+		// write results
 		writeResults(result);
 	}
 
+	/**
+	 * Return string for results from time calculation as a table.
+	 * @param times input data to be used in the results.
+	 */
 	private String parseTimeCalculations(List<String> times) {
 		String results = " ";
+
+		// divides each row in the table
 		String divider = String.format("%-60s", "|").replace(' ', '-') + "|\n";
 
+		// construct the table as a string
 		results += String.format("%60s", "\n").replace(' ', '-')
 			+ String.format("%-60s", "| Time (milliseconds)")
 				+ "|\n" + divider
@@ -83,13 +116,22 @@ public class ReportGenerator {
 			+ String.format("%-60s", "| Loop Strategy   | " + times.get(2))
 				+ "|\n "
 			+ String.format("%60s", "\n").replace(' ', '-');
+
+		// return the table
 		return results;
 	}
 
+	/**
+	 * Return string for results from the memory calculations as a table.
+	 * @param memories input data to be used in the results.
+	 */
 	private String parseMemoryCalculations(List<String> memories) {
 		String results = " ";
+
+		// divides each row in the table
 		String divider = String.format("%-60s", "|").replace(' ', '-') + "|\n";
 
+		// construct the table as a string
 		results += String.format("%60s", "\n").replace(' ', '-')
 			+ String.format("%-60s", "| Memory (megaflops)")
 				+ "|\n" + divider + "|"
@@ -111,9 +153,16 @@ public class ReportGenerator {
 				+ String.format("%-12s", memories.get(8))
 					+ "|\n "
 			+ String.format("%60s", "\n").replace(' ', '-');
+
+		// return the table
 		return results;
 	}
 
+	/**
+	 * Writes the output string to the output file specified from the
+	 * arguments used to run the program.
+	 * @param results output to write to file.
+	 */
 	private void writeResults(String results) {
 		out.flush();
 		out.write(results);
